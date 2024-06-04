63076e03e0f820bff4e9a953ead21ccf28149abd
heshammohamed <heshammuh@gmail>
2024-06-04 17:28:31 +0300

    splash View is done

--- lib/splash/presentation/view/widget/splash_view_body.dart
+++ lib/splash/presentation/view/widget/splash_view_body.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class SplashViewBody extends StatelessWidget {
-  const SplashViewBody({super.key});
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  _SplashViewBodyState createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  // video controller
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.asset(
+      'assets/pixels_spalsh.mp4',
+    )
+      ..initialize().then((_) {
+        setState(() {});
+      })
+      ..setVolume(0.0);
+
+    _playVideo();
+  }
+
+  void _playVideo() async {
+    // playing video
+    _controller.play();
+
+    //add delay till video is complite
+    await Future.delayed(const Duration(seconds: 6));
+
+    // navigating to home screen
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(
+                  _controller,
+                ),
+              )
+            : Container(),
+      ),
+    );
   }
 }
