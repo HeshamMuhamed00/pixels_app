7f3d94e1af945ebacd19b5530369d337e86d632b
heshammohamed <heshammuh@gmail>
2024-06-06 23:09:05 +0300

    add image in image details view

--- lib/home/presentation/view/widget/image_details.dart
+++ lib/home/presentation/view/widget/image_details.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ImageDetails extends StatelessWidget {
   const ImageDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Image.asset('assets/pexels.jpg'));
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Image.asset(
+          'assets/pexels.jpg',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
