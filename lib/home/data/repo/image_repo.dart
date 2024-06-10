67fc79262367c7e739b8df4472d0b5c846593d1b
heshammohamed <heshammuh@gmail>
2024-06-10 14:04:11 +0300

    create fetch image method in repo file

--- lib/home/data/repo/image_repo.dart
+++ lib/home/data/repo/image_repo.dart
@@ -1,1 +1,7 @@
-abstra
+import 'package:dartz/dartz.dart';
+import 'package:pixels_app/core/error/failure.dart';
+import 'package:pixels_app/home/data/model/images_model.dart';
+
+abstract class ImageRepo {
+  Future<Either<Failure, List<ImageModels>>> fetchImages();
+}
