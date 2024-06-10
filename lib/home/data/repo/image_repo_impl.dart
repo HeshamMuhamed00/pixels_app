3c65a9e5d20fbfb56dc3d77f25bcb6d8018f3c81
heshammohamed <heshammuh@gmail>
2024-06-10 15:54:59 +0300

    fetch image impl method is done

--- lib/home/data/repo/image_repo_impl.dart
+++ lib/home/data/repo/image_repo_impl.dart
@@ -1,10 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:pixels_app/core/error/failure.dart';
+import 'package:pixels_app/core/utils/api_service.dart';
 import 'package:pixels_app/home/data/model/images_model.dart';
 import 'package:pixels_app/home/data/repo/image_repo.dart';
 
 class ImageRepoImpl implements ImageRepo {
+  final ApiService apiService;
+
+  ImageRepoImpl(this.apiService);
+
   @override
   Future<Either<Failure, List<ImageModels>>> downloadImages(
       {required String imageUrl,
@@ -16,9 +21,19 @@ class ImageRepoImpl implements ImageRepo {
 
   @override
   Future<Either<Failure, List<ImageModels>>> fetchImages(
-      {required int? pageNumber}) {
-    // TODO: implement fetchImages
-    throw UnimplementedError();
+      {required int? pageNumber}) async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'curated?per_page=80&page=$pageNumber');
+      List<ImageModels> images = [];
+
+      for (int i = 0; i < data.length; i++) {
+        images.add(ImageModels.fromJson(data[i]));
+      }
+      return right(images);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
