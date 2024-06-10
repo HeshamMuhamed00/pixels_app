861f1b048e983c105fcc69cbc886b70f826717da
heshammohamed <heshammuh@gmail>
2024-06-10 13:53:51 +0300

    images model is done

--- lib/home/data/model/images_model.dart
+++ lib/home/data/model/images_model.dart
@@ -1,1 +1,23 @@
-class ImageModels {}
+class ImageModels {
+  final int imageID;
+  final String imageAlt;
+  final String imagePotraitPath;
+
+  const ImageModels({
+    required this.imageID,
+    required this.imageAlt,
+    required this.imagePotraitPath,
+  });
+
+  factory ImageModels.fromJson(Map<String, dynamic> json) => ImageModels(
+        imageID: json["id"] as int,
+        imageAlt: json["alt"] as String,
+        imagePotraitPath: json["src"]["portrait"] as String,
+      );
+
+  ImageModels.emptyConstructor({
+    this.imageID = 0,
+    this.imageAlt = '',
+    this.imagePotraitPath = '',
+  });
+}
