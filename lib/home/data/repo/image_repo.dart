a8654fc23f57d6fe58e5f4d422eaa9a332ad742e
heshammohamed <heshammuh@gmail>
2024-06-10 14:20:27 +0300

    methods in repo file is done

--- lib/home/data/repo/image_repo.dart
+++ lib/home/data/repo/image_repo.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
 import 'package:pixels_app/core/error/failure.dart';
 import 'package:pixels_app/home/data/model/images_model.dart';
 
@@ -7,4 +8,8 @@ abstract class ImageRepo {
       {required int? pageNumber});
   Future<Either<Failure, List<ImageModels>>> fetchImagesBySearch(
       {required String query});
+  Future<Either<Failure, List<ImageModels>>> downloadImages(
+      {required String imageUrl,
+      required int imageId,
+      required BuildContext context});
 }
