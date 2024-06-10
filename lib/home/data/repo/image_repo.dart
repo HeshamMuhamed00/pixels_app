d578f3671930cc291602a5181cc864dea9622633
heshammohamed <heshammuh@gmail>
2024-06-10 14:18:36 +0300

    create search method in repo file

--- lib/home/data/repo/image_repo.dart
+++ lib/home/data/repo/image_repo.dart
@@ -3,5 +3,8 @@ import 'package:pixels_app/core/error/failure.dart';
 import 'package:pixels_app/home/data/model/images_model.dart';
 
 abstract class ImageRepo {
-  Future<Either<Failure, List<ImageModels>>> fetchImages();
+  Future<Either<Failure, List<ImageModels>>> fetchImages(
+      {required int? pageNumber});
+  Future<Either<Failure, List<ImageModels>>> fetchImagesBySearch(
+      {required String query});
 }
