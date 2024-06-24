721020312af389dddc38ff6ab92cf2ab649d19f9
heshammohamed <heshammuh@gmail>
2024-06-24 14:32:29 +0300

    custom fading animation is don

---
+++ lib/core/widget/custom_fading_animation.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomFadingAnimation extends StatefulWidget {
+  const CustomFadingAnimation({super.key, required this.child});
+  final Widget child;
+
+  @override
+  State<CustomFadingAnimation> createState() => _CustomFadingAnimationState();
+}
+
+class _CustomFadingAnimationState extends State<CustomFadingAnimation>
+    with SingleTickerProviderStateMixin {
+  late Animation animation;
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    animationController = AnimationController(vsync: this);
+
+    animation =
+        Tween<double>(begin: 0.2, end: 0.8).animate(animationController);
+    animationController.addListener(() {
+      setState(() {});
+    });
+    animationController.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: animation.value,
+      child: widget.child,
+    );
+  }
+}
